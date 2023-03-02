c7331cc00fc1a79ee39df3304337714ec4dacfc1
martynlodder <102233130+martynlodder@users.noreply.github.com>
2023-03-02 16:07:14 +0100

    GoMail functionaliteiten toegevoegd

--- automailer.go
+++ automailer.go
@@ -5,40 +5,85 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
 	"log"
 	"net/smtp"
 	"os"
+	"path/filepath"
+
+	"github.com/go-mail/mail"
 )
 
-type Config struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+// Configuration struct for reading from config file
+type Configuration struct {
+    SMTPServer   string `json:"smtp_server"`
+    SMTPPort     int    `json:"smtp_port"`
+    Username     string `json:"username"`
+    Password     string `json:"password"`
+    Recipient    string `json:"recipient"`
+    Subject      string `json:"subject"`
+    Body         string `json:"body"`
+    Attachment   string `json:"attachment"`
+}
+
+// Read config from file
+func readConfig() Configuration {
+    configFile, err := os.Open("config.json")
+    if err != nil {
+        log.Fatalf("Failed to open config file: %v", err)
+    }
+    defer configFile.Close()
+
+    configBytes, err := ioutil.ReadAll(configFile)
+    if err != nil {
+        log.Fatalf("Failed to read config file: %v", err)
+    }
+
+    var config Configuration
+    err = json.Unmarshal(configBytes, &config)
+    if err != nil {
+        log.Fatalf("Failed to parse config file: %v", err)
+    }
+
+    return config
 }
 
 func main() {
-	// Open the config file
-	configFile, err := os.Open("config.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer configFile.Close()
+    // Read configuration from file
+    config := readConfig()
 
-	// Parse the JSON file
-	var config Config
-	err = json.NewDecoder(configFile).Decode(&config)
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Read attachment file
+    attachmentFile, err := os.Open(config.Attachment)
+    if err != nil {
+        log.Fatalf("Failed to open attachment file: %v", err)
+    }
+    defer attachmentFile.Close()
 
-	// Set up the authentication information.
-	auth := smtp.PlainAuth("", config.Email, config.Password, "smtp.office365.com")
+    attachmentBytes, err := ioutil.ReadAll(attachmentFile)
+    if err != nil {
+        log.Fatalf("Failed to read attachment file: %v", err)
+    }
 
-	// Connect to the server, authenticate, set the sender and recipient,
-	// and send the email all in one step.
-	err = smtp.SendMail("smtp.office365.com:587", auth, config.Email, []string{"PCN@student.fontys.nl"}, []byte("Subject: Test email\n\nThis is a test email."))
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Email sent successfully!")
+    attachment := mail.Attachment{
+        Name:        filepath.Base(config.Attachment),
+        Content:     attachmentBytes,
+        ContentType: "application/octet-stream",
+    }
+
+    // Create email message
+    m := mail.NewMessage()
+    m.SetHeader("From", config.Username)
+    m.SetHeader("To", config.Recipient)
+    m.SetHeader("Subject", config.Subject)
+    m.SetBody("text/plain", config.Body)
+    m.Attach(attachment)
+
+    // Connect to SMTP server and send email
+    auth := smtp.PlainAuth("", config.Username, config.Password, config.SMTPServer)
+    err = mail.SendMail(config.SMTPServer+":"+string(config.SMTPPort), auth, m)
+    if err != nil {
+        log.Fatalf("Failed to send email: %v", err)
+    }
+
+    log.Println("Email sent successfully")
 }
